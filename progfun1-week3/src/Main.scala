13eaa6e167babd933792dca17ab6c33559bf7b3e
Romina Espinosa <romina.espinosa@wizeline.com>
2017-07-05 13:05:39 -0500

    Add binary tree object with rest params

--- progfun1-week3/src/Main.scala
+++ progfun1-week3/src/Main.scala
@@ -1,4 +1,4 @@
-import presistent.binary_tree.EmptyLeaf
+import presistent.BinaryTree
 import scala.util.Random
 
 object Main extends App {
@@ -8,11 +8,11 @@ object Main extends App {
   println("- Classes and Abstract Classes can have fields (def) and params (val, var). Traits can only have fields.")
   println("")
   
-  val pairs = EmptyLeaf + 2 + 4 + 6 + 8 + 10
+  val pairs = BinaryTree(2, 4, 6, 8 , 10)
     
-  val nones = EmptyLeaf + 1 + 3 + 5 + 7 + 9
+  val nones = BinaryTree(1, 3, 5, 7, 9)
     
-  val primes = EmptyLeaf + 2 + 3 + 5 + 7
+  val primes = BinaryTree(2, 3, 5, 7)
   
   println(s"Pairs contains 5 ? ${ pairs contains 5 }")
   println(s"Pairs u Nones contains 5 ? ${(pairs u nones) contains 5 }")
--- progfun1-week3/src/presistent/BinaryTree.scala
+++ progfun1-week3/src/presistent/BinaryTree.scala
@@ -1,5 +1,13 @@
 package presistent
 
-abstract class BinaryTree[T <: AnyVal] {
+import presistent.binary_tree.EmptyLeaf
 
+object BinaryTree {
+  def apply(elems: Int*):BinTreeNode[Int] = {
+    var tree:BinTreeNode[Int] = EmptyLeaf
+    elems.foreach { e =>
+      tree = tree + e
+    }
+    return tree
+  }
 }
